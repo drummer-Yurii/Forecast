9f8be01e4404e002e54ac62bbb7428438516cc61
Yurii Rybachok <drummer2009@ukr.net>
2023-05-06 17:22:00 +0300

    show data from api in highlights component

--- src/App.vue
+++ src/App.vue
@@ -38,7 +38,7 @@ onMounted(getWeather)
               </div>
             </section>
             <section class="section section-right">
-              <Highlights />
+              <Highlights :weatherInfo="weatherInfo" />
             </section> 
           </div>
           <div v-if="weatherInfo?.weather" class="sections">
